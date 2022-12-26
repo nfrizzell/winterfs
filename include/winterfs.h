6801e2c850b90d3d8a926006344cb9773da3d91c
nfrizzell <54083373+nfrizzell@users.noreply.github.com>
2022-12-25 20:29:36 -0500

    more spec

--- include/winterfs.h
+++ include/winterfs.h
@@ -1,9 +1,11 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 
+#define WINTERFS_MAGIC		0x574e4653
+
 #define WINTERFS_BLOCK_SIZE 	4096 // fixed for now...
 
-#define WINTERFS_SB_SIZE	WINTERFS_BLOCK_SIZE
+#define WINTERFS_SUPERBLOCK_LBA		1
 
 #define WINTERFS_INODE_PRIMARY_BLOCKS 	8
 #define WINTERFS_INODE_SIZE 		128
@@ -13,28 +15,48 @@
 #define WINTERFS_BYTES_PER_INODE	(15 << 1)
 #define WINTERFS_INODES_PER_BLOCK	WINTERFS_BLOCK_SIZE / WINTERFS_INODE_SIZE
 
-typedef __le64 winterfs_block_addr;
-
+// superblock on-disk structure
 struct winterfs_superblock {
         __le64 num_inodes;
         __le64 num_blocks;
+	__le64 first_inode_block;
+	__le64 first_data_block;
         u8 block_size; // actual size = 2^block_size
-	u8 pad[4079];
 } __attribute__((packed));
 
+// superblock in-memory data
+struct winterfs_sb_info {
+	u64 num_inodes;
+	u64 num_blocks;
+	u64 first_inode_block;
+	u64 first_data_block;
+	struct super_block *vfs_sb;
+	spinlock_t s_lock;
+	u8 block_size;
+};
+
 struct winterfs_indirect_block_list {
-	__le64 blocks[WINTERFS_BLOCK_SIZE / sizeof(__le64)];
+	__le32 blocks[WINTERFS_BLOCK_SIZE / sizeof(__le32)];
 } __attribute__((packed));
 
 struct winterfs_inode {
 	__le64 size;
         u8 type;
 	u8 pad[39]; // reserved for metadata
-	winterfs_block_addr primary_blocks[WINTERFS_INODE_PRIMARY_BLOCKS];
-        winterfs_block_addr secondary_blocks;
-	winterfs_block_addr tertiary_blocks;
+	__le64 primary_blocks[WINTERFS_INODE_PRIMARY_BLOCKS];
+        __le64 secondary_blocks;
+	__le64 tertiary_blocks;
+} __attribute__((packed));
+
+struct winterfs_inode_info {
+	u64 size;
+        u8 type;
+	u64 primary_blocks[WINTERFS_INODE_PRIMARY_BLOCKS];
+        u64 secondary_blocks;
+	u64 tertiary_blocks;
 } __attribute__((packed));
 
 struct winterfs_free_blocks {
 	u8 *bitset;
 } __attribute__((packed));
+
--- winterfs/super.c
+++ winterfs/super.c
@@ -1,17 +1,42 @@
+#include <linux/buffer_head.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 #include "winterfs.h"
 
 static int winterfs_fill_super(struct super_block *sb, void *data, int silent)
 {
+	int ret;
+	struct winterfs_superblock ws;
+	struct winterfs_sb_info *sbi;
+	struct buffer_head *sb_buf;
+
+	sbi = kzalloc(sizeof(struct winterfs_sb_info), GFP_KERNEL);
+	if (!sbi) {
+		return ENOMEM;
+	}
+
+	spin_lock_init(&(sbi->s_lock));
+	sbi->vfs_sb = sb;
+	sb->s_fs_info = sbi;
+
+	sb_buf = sb_bread(sb, WINTERFS_SUPERBLOCK_LBA);
+	if (!sb_buf) {
+		printk(KERN_ERR "Error reading superblock from disk");
+		goto err;
+	}
+
 	return 0;
+err:
+	sb->s_fs_info = NULL;
+	kfree(sbi);
+	return ret;	
 }
 
 static struct dentry *winterfs_mount(struct file_system_type *fs_type,
         int flags, const char *dev_name, void *data)
 {
-	printk(KERN_ALERT "Mounting winterfs");
 	return mount_bdev(fs_type, flags, dev_name, data, winterfs_fill_super);
 }
 
@@ -20,16 +45,18 @@ const struct super_operations winterfs_super_operations = {
 };
 
 static struct file_system_type winterfs_fs_type = {
-	.mount	= winterfs_mount,
-	.name	= "winterfs",
-	.owner	= THIS_MODULE,
+	.owner			= THIS_MODULE,
+	.name			= "winterfs",
+	.mount			= winterfs_mount,
+	.kill_sb		= kill_block_super,
+	.fs_flags		= FS_REQUIRES_DEV
 };
 
 static int __init init_winterfs_fs(void)
 {
 	int err;
 
-	BUILD_BUG_ON(sizeof(struct winterfs_superblock) != WINTERFS_SB_SIZE);
+	BUILD_BUG_ON(sizeof(struct winterfs_superblock) > WINTERFS_BLOCK_SIZE);
 	BUILD_BUG_ON(sizeof(struct winterfs_indirect_block_list) != WINTERFS_BLOCK_SIZE);
 	BUILD_BUG_ON(sizeof(struct winterfs_inode) != WINTERFS_INODE_SIZE);
 
