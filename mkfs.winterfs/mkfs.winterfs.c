58d37313ee804445b6823994feb534cb09d6a8fb
nfrizzell <54083373+nfrizzell@users.noreply.github.com>
2022-12-27 15:52:45 -0500

    fix magic endianness

--- mkfs.winterfs/mkfs.winterfs.c
+++ mkfs.winterfs/mkfs.winterfs.c
@@ -8,8 +8,6 @@
 
 #define WINTERFS_BLOCK_SIZE     4096
 
-#define WINTERFS_MAGIC          0x574e4653
-
 #define WINTERFS_SUPERBLOCK_LBA         1
 
 #define WINTERFS_INODE_PRIMARY_BLOCKS   8
@@ -48,49 +46,51 @@ uint64_t le64(uint32_t val)
 }
 
 struct winterfs_inode {
-        uint64_t size;
-        uint8_t type;
-        uint8_t pad[39]; // reserved for metadata
-        uint64_t primary_blocks[WINTERFS_INODE_PRIMARY_BLOCKS];
-        uint64_t secondary_blocks;
-        uint64_t tertiary_blocks;
+	uint64_t size;
+	uint8_t type;
+	uint8_t pad[39]; // reserved for metadata
+	uint64_t primary_blocks[WINTERFS_INODE_PRIMARY_BLOCKS];
+	uint64_t secondary_blocks;
+	uint64_t tertiary_blocks;
 } __attribute__((packed));
 
 struct winterfs_block_list {
-        uint64_t size; // number of items in the list
-        uint64_t num_nodes; // number of blocks the list occupies
-        uint64_t list_head; // block index of the first node
-        uint64_t list_tail; // block index of the last node
+	uint64_t size; // number of items in the list
+	uint64_t num_nodes; // number of blocks the list occupies
+	uint64_t list_head; // block index of the first node
+	uint64_t list_tail; // block index of the last node
 } __attribute__((packed));
 
 struct winterfs_free_list_node {
-        uint64_t list[(WINTERFS_BLOCK_SIZE / sizeof(uint64_t)) - 1];
-        uint64_t next;
+	uint64_t list[(WINTERFS_BLOCK_SIZE / sizeof(uint64_t)) - 1];
+	uint64_t next;
 } __attribute__((packed));
 
 struct winterfs_inode_list_node {
-        struct winterfs_inode list[(WINTERFS_BLOCK_SIZE / WINTERFS_INODE_SIZE) - 1];
-        uint8_t pad[WINTERFS_INODE_SIZE - sizeof(uint64_t)];
-        uint64_t next;
+	struct winterfs_inode list[(WINTERFS_BLOCK_SIZE / WINTERFS_INODE_SIZE) - 1];
+	uint8_t pad[WINTERFS_INODE_SIZE - sizeof(uint64_t)];
+	uint64_t next;
 } __attribute__((packed));
 
 struct winterfs_superblock {
-        uint32_t magic;
-        uint64_t num_inodes;
-        uint64_t num_blocks;
+	uint8_t magic[4];
+	uint64_t num_inodes;
+	uint64_t num_blocks;
 
-        struct winterfs_block_list free_block_list;
-        struct winterfs_block_list inode_list;
+	struct winterfs_block_list free_block_list;
+	struct winterfs_block_list inode_list;
 
-        uint8_t block_size; // actual size = 2^block_size
+	uint8_t block_size; // actual size = 2^block_size
 } __attribute__((packed));
 
 int write_superblock(FILE *dev, struct stat *s)
 {
-	struct winterfs_superblock sb;
-	sb.magic = WINTERFS_MAGIC;
-	sb.num_blocks = s->st_size / WINTERFS_BLOCK_SIZE + (s->st_size % WINTERFS_BLOCK_SIZE != 0);
-	sb.num_inodes = 31;
+	struct winterfs_superblock sb = {
+		.magic = {0x57, 0x4e, 0x46, 0x53},
+		.num_blocks = s->st_size / WINTERFS_BLOCK_SIZE + (s->st_size % WINTERFS_BLOCK_SIZE != 0),
+		.num_inodes = 31
+	};
+
 	fseek(dev, 0, SEEK_SET);
 	if(!fwrite(&sb, sizeof(sb), 1, dev)) {
 		printf("Failed writing superblock\n");
